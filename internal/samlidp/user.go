5e631f584ea6550e4c74e44aad96656849bd8f70
Kudryavtsev Kirill <rtutiui@mail.ru>
2023-02-08 00:04:42 +0300

    add handler for get list users

--- internal/samlidp/user.go
+++ internal/samlidp/user.go
@@ -1,5 +1,10 @@
 package samlidp
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
 type User struct {
 	Name              string   `json:"name"`
 	PlaintextPassword *string  `json:"password,omitempty"` // not stored
@@ -11,3 +16,25 @@ type User struct {
 	GivenName         string   `json:"given_name,omitempty"`
 	ScopedAffiliation string   `json:"scoped_affiliation,omitempty"`
 }
+
+// HandlerListUsers обрабатывает запрос `GET /api/v1/users/` и отвечает списком имен пользователей в формате JSON.
+func (s *Server) HandlerListUsers(c *gin.Context) {
+	w := c.Writer
+
+	users, err := s.Store.List("/users/")
+	if err != nil {
+		s.logger.Printf("ERROR: %S", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	c.JSON(http.StatusOK, struct {
+		Users []string `json:"users"`
+	}{Users: users})
+}
+
+func (s *Server) HandleGetUser(c *gin.Context) {}
+
+func (s *Server) HandlePutUser(c *gin.Context) {}
+
+func (s *Server) HandleDeleteUser(c *gin.Context) {}
