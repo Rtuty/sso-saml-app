3b44869c154e13b711d7e4686adc73bc74deef11
Kirill Kudryavcev <rtutiui@mail.ru>
2023-02-07 16:38:43 +0300

    add delete and list methods for memory storage struct/storage global interface

--- internal/samlidp/memory_storage.go
+++ internal/samlidp/memory_storage.go
@@ -3,6 +3,7 @@ package samlidp
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 	"sync"
 )
 
@@ -42,6 +43,23 @@ func (s *MemoryStore) Put(key string, value interface{}) error {
 	return nil
 }
 
-func (s *MemoryStore) Delete(key string) error { return nil }
+// Delete удаляет key
+func (s *MemoryStore) Delete(key string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.data, key)
+	return nil
+}
 
-func (s *MemoryStore) List(prefix string) ([]string, error) { return []string{""}, nil }
+/*
+Список возвращает все ключи, которые начинаются с `prefix`.
+Префикс удаляется из каждого возвращаемого значения. Например, если ключи ["aa", "ab", "cd"], то List("a") выдаст []строку {"a", "b"} */
+func (s *MemoryStore) List(prefix string) ([]string, error) {
+	rv := []string{}
+	for k := range s.data {
+		if strings.HasPrefix(k, prefix) {
+			rv = append(rv, strings.TrimPrefix(k, prefix))
+		}
+	}
+	return rv, nil
+}
