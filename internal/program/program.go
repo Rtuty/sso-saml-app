6428bf6053a2520fb1fefa93e884b517fff979e6
Kudryavtsev Kirill <rtutiui@mail.ru>
2023-02-12 22:18:36 +0300

    add get base url for new program func

--- internal/program/program.go
+++ internal/program/program.go
@@ -1,13 +1,16 @@
 package program
 
 import (
+	"fmt"
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/contrib/secure"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
+	"log"
 	"modules/internal/logger"
 	"modules/internal/samlidp"
 	"net/http"
+	"net/url"
 	"path/filepath"
 )
 
@@ -20,6 +23,15 @@ type Program struct {
 
 func New(cfg *viper.Viper, logger *logger.LoggerEx, workDir string) *Program {
 	p := new(Program)
+	host := p.cfg.GetString("server.host")
+	port := p.cfg.GetInt("server.port")
+
+	baseURLstr := fmt.Sprintf(`http://%s:%d`, host, port)
+	baseURL, err := url.Parse(baseURLstr)
+	if err != nil {
+		log.Fatalf("cannot parse base URL: %v", err)
+		return nil
+	}
 
 	gin.SetMode(gin.ReleaseMode)
 	gin.DisableConsoleColor()
